ae8bfa5c4e3d6b344d94e222726144fb38625374
Mario Uher <uher.mario@gmail.com>
2015-11-17 23:09:37 +0100

    Add eslint for webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,40 @@
-var webpack = require('webpack');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var ReactToHtmlPlugin = require('react-to-html-webpack-plugin');
+/*eslint-env node*/
 
-var path = require('path');
-var ejs = require('ejs');
-var fs = require('fs');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var ReactToHtmlPlugin = require("react-to-html-webpack-plugin");
+
+var ejs = require("ejs");
+var fs = require("fs");
 
 
 module.exports = {
-  entry: './components/Terminal/index.js',
+  entry: "./components/Terminal/index.js",
 
   output: {
     path: __dirname,
-    filename: 'index.js',
-    libraryTarget: 'umd'
+    filename: "index.js",
+    libraryTarget: "umd"
   },
 
   module: {
     loaders: [
-      { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader') },
-      { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel?stage=0&loose[]=es6.modules&loose[]=es6.classes' }
+      { test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader") },
+      { test: /\.jsx?$/, exclude: /node_modules/, loader: "babel?stage=0&loose[]=es6.modules&loose[]=es6.classes" }
     ]
   },
 
   postcss: [
-    require('autoprefixer-core'),
-    require('postcss-calc'),
-    require('postcss-color-function'),
-    require('postcss-custom-properties'),
-    require('postcss-custom-media')
+    require("autoprefixer-core"),
+    require("postcss-calc"),
+    require("postcss-color-function"),
+    require("postcss-custom-properties"),
+    require("postcss-custom-media")
   ],
 
   plugins: [
-    new ExtractTextPlugin('style.css', { allChunks: true }),
-    new ReactToHtmlPlugin('index.html', 'index.js', {
-      template: ejs.compile(fs.readFileSync(__dirname + '/index.ejs', 'utf-8'))
+    new ExtractTextPlugin("style.css", { allChunks: true }),
+    new ReactToHtmlPlugin("index.html", "index.js", {
+      template: ejs.compile(fs.readFileSync(__dirname + "/index.ejs", "utf-8"))
     })
   ]
 };
