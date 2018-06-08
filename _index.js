e511cc63bb02a7a6e9777010fab97958ecac693d
Mario Uher <uher.mario@gmail.com>
2018-06-08 10:02:33 +0200

    Rename components to src

--- _index.js
+++ _index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDom from "react-dom";
-import App from "./components/App";
+import App from "./src/App";
 
 ReactDom.render(<App />, document.querySelector("main"));
